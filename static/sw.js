dfbe96270cbdd0f56a2e5902ab8cf2841440ce97
soal <soal.red@pm.me>
2025-12-26 02:42:57 +0300

    Fixed checkbox styling

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v6";
+const VERSION = "v7";
 const PRECACHE = [
   "/",
   "/app.css",
@@ -62,27 +62,13 @@ const putInCache = async (request, response) => {
   await cache.put(request, response);
 };
 
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+const cacheFirst = async ({ request, fallbackUrl }) => {
   // First try to get the resource from the cache
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
     return responseFromCache;
   }
 
-  // Next try to use the preloaded response, if it's there
-  // NOTE: Chrome throws errors regarding preloadResponse, see:
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
-  // https://github.com/mdn/dom-examples/issues/145
-  // To avoid those errors, remove or comment out this block of preloadResponse
-  // code along with enableNavigationPreload() and the "activate" listener.
-  // const preloadResponse = await preloadResponsePromise;
-  // if (preloadResponse) {
-  //   console.info("using preload response", preloadResponse);
-  //   putInCache(request, preloadResponse.clone());
-  //   return preloadResponse;
-  // }
-
-  // Next try to get the resource from the network
   try {
     const responseFromNetwork = await fetch(request.clone());
     // response may be used only once
@@ -107,15 +93,7 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   }
 };
 
-// const enableNavigationPreload = async () => {
-//   if (self.registration.navigationPreload) {
-//     // Enable navigation preloads!
-//     await self.registration.navigationPreload.enable();
-//   }
-// };
-
 self.addEventListener("activate", (event) => {
-  // event.waitUntil(enableNavigationPreload());
   const currentCaches = [PRECACHE];
   event.waitUntil(
     caches
